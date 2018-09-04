cfcd20bc8760b6de442b5c92d79f6dc0e1671873
Luis Perez <luis.perez@bluewolf.com.mx>
2018-09-04 17:03:54 -0600

    cambios intereses.php 1.0.2
    Se esta agregando la informacion necesaria para la empresa interesada

--- Intereses.php
+++ Intereses.php
@@ -28,7 +28,10 @@
 				z-index: 9999;
 				background: url('images/loader.gif') 50% 50% no-repeat rgb(249,249,249);
 				opacity: .8;
-			}
+            }
+            .borlef{
+                border-right: 5px solid #14234b;
+            }
         </style>
         <script>
             $(document).ready(function(){
@@ -45,7 +48,7 @@
         </div> -->
         <?
             $count=0;
-            $Consulta_espana = "SELECT * FROM espana";
+            $Consulta_espana = "SELECT * FROM espana ORDER BY id_espana";
             $ResultadoConsulta = mysql_query($Consulta_espana) or die("La consulta a espana : $Consulta_espana" . mysql_error());
             while ($ResConsulta = mysql_fetch_assoc($ResultadoConsulta))
             {
@@ -53,7 +56,7 @@
                 $IdEspana = $ResConsulta['id_espana'];
         ?>
                 <!-- Empresa <?echo $count;?> -->
-                <div id="empresa<?echo $count;?>" class="container">
+                <div id="empresa<?echo $count;?>" class="">
 
                         <!-- Nombre empresa <?echo $count;?>-->
                         <form method="post" name="form<?echo $count;?>" id="form<?echo $count;?>">
@@ -70,36 +73,30 @@
 
                                     <!-- productos -->
                                     <div class="row">
-                                        <span class="black-text center">
-                                            <h5>Descripción</h5>   
-                                        </span>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col s12 m12 l12 xl12 center">
-                                            <span class="center">
-                                                <?echo $ResConsulta['productos'];?>
+                                        <div class="col s12 m12 l6 xl6 borlef">
+                                            <span class="black-text left-align">
+                                                <h5>Descripción</h5>   
+                                            </span>
+                                        
+                                            <span class="left-align">
+                                                <?echo nl2br($ResConsulta['descripcion']);?>
                                             </span>
                                         </div>
-                                    </div>
-                                    <!--fin  productos -->
+                                        
+                                        <div class="col s12 m12 l6 xl5 offset-xl1">
+                                            <span class="black-text left-align">
+                                                <h5>Intereses</h5>   
+                                            </span>
 
-                                    <!-- intereses -->
-                                    <div class="row">
-                                        <span class="black-text center">
-                                            <h5>Intereses</h5>   
-                                        </span>
-                                    </div>
-
-                                    <?php
-                                        $CountIntereses=0;
-                                        $ConsultaIntereses = "SELECT * FROM espana_intereses WHERE id_espana=$IdEspana";
-                                        $ResultadoIntereses = mysql_query($ConsultaIntereses) or die("La consulta a interereses : $ConsultaIntereses" . mysql_error());
-                                        while ($ResIntereses = mysql_fetch_assoc($ResultadoIntereses))
-                                        {
-                                            $CountIntereses++;
-                                    ?>
-                                            <div class="row">
-                                                <div class="col s12 m12 l6 xl6 offset-xl4 offset-l4">
+                                            <?php
+                                                $CountIntereses=0;
+                                                $ConsultaIntereses = "SELECT * FROM espana_intereses WHERE id_espana=$IdEspana";
+                                                $ResultadoIntereses = mysql_query($ConsultaIntereses) or die("La consulta a interereses : $ConsultaIntereses" . mysql_error());
+                                                while ($ResIntereses = mysql_fetch_assoc($ResultadoIntereses))
+                                                {
+                                                    $CountIntereses++;
+                                            ?>
+                                            
                                                     <p class="left-align">
                                                         <label>
                                                             <input type="checkbox" name="interes[]" id="interes<?echo $CountIntereses?>"/>
@@ -108,13 +105,13 @@
                                                             </span>
                                                         </label>
                                                     </p>
-                                                </div>
-                                            </div>
-                                    <?
-                                        }
-                                    ?>
-                                    <!--fin  intereses -->
-
+                                               
+                                            <?
+                                                }
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <!--fin  productos -->
                                 </div>
                             </div>
                         </form>
@@ -124,6 +121,43 @@
         <?php
             }
         ?>
+        
+        <!-- informacion de empresa interesada -->
+        <div id="infoempresa" class="">
+            <form method="post" name="empresainteresada" id="empresainteresada">
+                <div class="row">
+                    <div class="col s12 m12 l12 xl12  z-depth-1">
+
+                        <div class="row">
+                            <div class="card-panel bw">
+                                <span class="black-text center">
+                                        <h5 class="white-text">Informacion necesaria</h5>   
+                                </span>
+                            </div>
+                        </div>  
+
+                        <div class="row">
+                            <div class="col s12 m12 l6 xl6 borlef">
+                                <!-- Nombre Empresa -->
+                                <div class="input-field col s12 m12 l12 xl12">
+                                    <input name="nombre" id="nombre" type="text" class="validate" required>
+                                    <label for="nombre">Nombre Empresa</label>
+                                </div>
+                            </div>
+
+                            <div class="col s12 m12 l6 xl6">
+                                <!-- Nombre Comercial -->
+                                <div class="input-field col s12 m12 l12 xl12">
+                                    <input name="comercial" id="comercial" type="text" class="validate" required>
+                                    <label for="comercial">Nombre Comercial</label>
+                                </div>
+                            </div>
+                        </div>                               
+                    </div>                
+                </div>
+            </form> 
+        </div>
+        <!-- fin de informacion de empresa interesada -->
 
         <!--JavaScript at end of body for optimized loading-->
         <script type="text/javascript" src="js/materialize.min.js"></script>
